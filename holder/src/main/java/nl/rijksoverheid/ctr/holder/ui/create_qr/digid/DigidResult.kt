3fe422b7149fd696a97d35c7755aa3906cb68e0f
Efe Kocabas <Efe.Kocabas@themobilecompany.com>
2021-08-19 12:51:38 +0300

    removing holder module

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/digid/DigidResult.kt
+++
@@ -1,6 +0,0 @@
-package nl.rijksoverheid.ctr.holder.ui.create_qr.digid
-
-sealed class DigidResult {
-    data class Success(val jwt: String): DigidResult()
-    data class Failed(val error: String?): DigidResult()
-}
